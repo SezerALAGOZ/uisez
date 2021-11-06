6cd26fd07cc49c9190e91cd3d954f9854304f448
Sezer Alagoz <sezeralagoz8@gmail.com>
2021-11-06 15:12:09 +0300

    npm publish done

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { ExampleComponent } from 'uisez'
+import { ButtonComponent } from 'uisez'
 import 'uisez/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+  <>
+  <ButtonComponent type="default" text="Button" />
+  <ButtonComponent type="primary" text="Button" />
+  <ButtonComponent type="text" text="Button" />
+  <ButtonComponent type="link" text="Button" />
+  <ButtonComponent type="dashed" text="Button" />
+  </>)
 }
 
-export default App
+export default App;
